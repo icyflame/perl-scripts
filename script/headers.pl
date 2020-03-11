42f92a5b67a67c1580cc1a8ada6c25492272cced
Siddharth Kannan <mail@siddharthkannan.in>
2020-03-11 23:51:43 +0900

    Remove documentation as it breaks the script itself

--- script/headers.pl
+++ script/headers.pl
@@ -1,56 +1,5 @@
 #!/usr/bin/env perl
 
-'''
-headers.pl
-
-> Convert nested Markdown into a markdown file with headers
-
-Usage: cat file.md | perl headers.pl <levels>
-
-Options:
-
-    <levels> => The highest level of nesting which should be turned into a
-    header in the resulting markdown
-
-Example:
-
-Input:
-
-- Title
-    - Subtitle
-        - Pros
-            - Pro 1
-            - Pro 2
-        - Cons
-            - Con 1
-    - Subtitle 2
-        - Pros
-            - Pro 3
-            - Pro 4
-        - Cons
-            - Con 2
-
-Run: cat file | perl headers.pl 2
-
-Result:
-
-# Title
-## Subtitle
-### Pros
-- Pro 1
-- Pro 2
-### Cons
-- Con 1
-## Subtitle 2
-### Pros
-- Pro 3
-- Pro 4
-### Cons
-- Con 2
-
-After rendering, markdown with headers looks better than nested markdown.
-'''
-
 use strict;
 use warnings;
 binmode STDOUT, ":utf8";
